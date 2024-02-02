79ebba55274722f67dba07d1219a976de7c94247
Nishant Verma <nishantverma040@iitbhilai.ac.in>
2024-02-03 01:22:25 +0530

    delete lambda config

--- handler.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const app = require('./app');
-const serverless = require('serverless-http');
-module.exports.hello = serverless(app);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-
